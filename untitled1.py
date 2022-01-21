932ed3c236763d353905a8203ebbd6018c635cb4
Prathmesh Karale <prathmesh.1501.k@gmail.com>
2022-01-21 13:28:35 +0530

    first commit

---
+++ untitled1.py
@@ -0,0 +1,16 @@
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "AOfdXONRF6NDBGflb1yOUyYqykFcfHJWXrObb6xEsTXu"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE: manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"input_data": [{"field": ['cab_type', 'name','product_id','source','destination'], "values": [[ 0,  9, 10,  6,  6]]}]}
+
+response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/ff9af1c7-709b-41f6-8525-e37ac1cbb07c/predictions?version=2022-01-20', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+print("Scoring response")
+predictions =response_scoring.json()
+print(predictions['predictions'][0]['values'][0][0])
